4d8454bc433cf09935250c945f1f49022b3b0071
Maxime <maximereach@gmail.com>
2023-02-16 20:28:02 +0100

    Ajout des premieres balises HTML, premiers style et test en JS pour voir si ça marche

---
+++ script.js
@@ -0,0 +1,7 @@
+let input = document.getElementById('nombre');
+let bouton = document.getElementById('bouton');
+
+bouton.addEventListener('click',console);
+function console() {
+    alert(input.value);
+}
